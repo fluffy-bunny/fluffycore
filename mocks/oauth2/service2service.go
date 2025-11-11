3e932fffbfac13d3e65caaf39f67d6f89b07cfe5
herb stahl <ghstahl@gmail.com>
2025-11-11 13:21:55 -0800

    rs256_minter

--- mocks/oauth2/service2service.go
+++ mocks/oauth2/service2service.go
@@ -19,22 +19,31 @@ import (
 type (
 	PublicJwk struct {
 		Alg string `json:"alg"`
-		Crv string `json:"crv"`
+		Crv string `json:"crv,omitempty"` // For EC keys
 		Kid string `json:"kid"`
 		Kty string `json:"kty"`
 		Use string `json:"use"`
-		X   string `json:"x"`
-		Y   string `json:"y"`
+		X   string `json:"x,omitempty"` // For EC keys
+		Y   string `json:"y,omitempty"` // For EC keys
+		E   string `json:"e,omitempty"` // For RSA keys
+		N   string `json:"n,omitempty"` // For RSA keys
 	}
 	PrivateJwk struct {
 		Alg string `json:"alg"`
-		Crv string `json:"crv"`
+		Crv string `json:"crv,omitempty"` // For EC keys
 		D   string `json:"d"`
 		Kid string `json:"kid"`
 		Kty string `json:"kty"`
 		Use string `json:"use"`
-		X   string `json:"x"`
-		Y   string `json:"y"`
+		X   string `json:"x,omitempty"`  // For EC keys
+		Y   string `json:"y,omitempty"`  // For EC keys
+		E   string `json:"e,omitempty"`  // For RSA keys
+		N   string `json:"n,omitempty"`  // For RSA keys
+		P   string `json:"p,omitempty"`  // For RSA keys
+		Q   string `json:"q,omitempty"`  // For RSA keys
+		Dp  string `json:"dp,omitempty"` // For RSA keys
+		Dq  string `json:"dq,omitempty"` // For RSA keys
+		Qi  string `json:"qi,omitempty"` // For RSA keys
 	}
 	SigningKey struct {
 		PrivateKey string     `json:"private_key"`
